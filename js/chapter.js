29399ce08eac4f5837a874acd5e6128d7666050a
darkworldhope <darkworldhope@gmail.com>
2026-06-27 23:52:43 +0530

    Update chapter.js

--- js/chapter.js
+++ js/chapter.js
@@ -1,30 +1,16 @@
 const params = new URLSearchParams(window.location.search);
 const chapter = params.get("name");
 
-document.getElementById("title").innerText = chapter || "Chapter";
+document.getElementById("title").innerText = chapter;
 
 document.getElementById("chapter-data").innerHTML = `
-<div class="card">
-🔥 ${chapter} Chapter
-</div>
+<div class="card">📚 Previous Year Questions</div>
 
-<div class="card">
-Previous Year Questions
-</div>
+<div class="card">⭐ Important Questions</div>
 
-<div class="card">
-Important Questions
-</div>
+<div class="card">📝 Notes</div>
 
-<div class="card">
-Notes
-</div>
+<div class="card">🖼 Images</div>
 
-<div class="card">
-Images
-</div>
-
-<div class="card">
-MCQs
-</div>
+<div class="card">❓ MCQs</div>
 `;
