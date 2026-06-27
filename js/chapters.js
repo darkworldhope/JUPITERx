2468f6ff6ec5501a0367a43255cd8896a5ffb388
darkworldhope <darkworldhope@gmail.com>
2026-06-27 23:20:58 +0530

    Create chapters.js

---
+++ js/chapters.js
@@ -0,0 +1,24 @@
+fetch("data/chapters.json")
+.then(response => response.json())
+.then(data => {
+
+let html = "";
+
+data.forEach(chapter => {
+
+html += `
+<div class="chapter"
+onclick="location.href='chapter.html?name=${chapter.chapter}'">
+
+${chapter.chapter}
+
+<span class="count">${chapter.count}</span>
+
+</div>
+`;
+
+});
+
+document.getElementById("chapter-list").innerHTML = html;
+
+});
