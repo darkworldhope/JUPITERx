b32ecbcb1dcde7648c1d7c62e8da71d00f5e0caf
darkworldhope <darkworldhope@gmail.com>
2026-06-29 03:49:47 +0530

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,27 @@
+const btn = document.getElementById("addSubject");
 
+btn.addEventListener("click", async () => {
+  const name = document.getElementById("subject").value.trim();
+  const yearId = parseInt(document.getElementById("year").value);
+
+  if (!name) {
+    alert("Please enter subject name");
+    return;
+  }
+
+  const { error } = await window.supabaseClient
+    .from("subjects")
+    .insert([
+      {
+        name: name,
+        year_id: yearId
+      }
+    ]);
+
+  if (error) {
+    alert(error.message);
+  } else {
+    alert("Subject Added Successfully ✅");
+    document.getElementById("subject").value = "";
+  }
+});
