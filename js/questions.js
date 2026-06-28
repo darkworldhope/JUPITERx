ab590029597f7ca9ac193703a83ef68fe08bc762
darkworldhope <darkworldhope@gmail.com>
2026-06-29 04:54:02 +0530

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -1,41 +1,4 @@
 const params = new URLSearchParams(window.location.search);
-
 const chapter = params.get("chapter");
-const year = Number(params.get("year"));
 
-document.getElementById("title").innerText =
-`${chapter} - ${year}`;
-
-fetch("data/pyqs.json")
-.then(response => response.json())
-.then(data => {
-
-const filtered = data.filter(item =>
-item.chapter === chapter && item.year === year
-);
-
-let html = "";
-
-filtered.forEach(item => {
-
-html += `
-<div class="question">
-
-<span class="count">${item.marks}M</span>
-
-<b>${item.type}</b><br><br>
-
-${item.question}
-
-</div>
-`;
-
-});
-
-if(filtered.length===0){
-html="<div class='question'>No PYQs Available</div>";
-}
-
-document.getElementById("question-list").innerHTML=html;
-
-});
+console.log("Chapter:", chapter);
