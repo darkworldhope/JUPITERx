696641afd5b0d34b2b334ca1a79f73bba821c351
darkworldhope <darkworldhope@gmail.com>
2026-06-29 03:17:44 +0530

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://pwsfigdqsgjyiioggdce.supabase.co";
+const SUPABASE_KEY = "sb_publishable_NBAGoITOg01ojvWB9IxcQw_c3WWZcr4";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
