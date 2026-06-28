692b40189a81160f1fde650e24890a5980a6ea75
darkworldhope <darkworldhope@gmail.com>
2026-06-29 05:06:01 +0530

    Update papers.js

--- js/papers.js
+++ js/papers.js
@@ -1,40 +1,44 @@
 const params = new URLSearchParams(window.location.search);
-const subject = params.get("subject");
+const subject = Number(params.get("subject"));
 
 const title = document.getElementById("title");
 const papers = document.getElementById("papers");
 
 loadPapers();
 
-async function loadPapers(){
+async function loadPapers() {
 
-const {data,error}=await window.supabaseClient
-.from("papers")
-.select("*")
-.eq("subject_id",Number(subject))
-.order("id");
+  const { data, error } = await window.supabaseClient
+    .from("papers")
+    .select("*")
+    .eq("subject_id", subject)
+    .order("id");
 
-if(error){
-papers.innerHTML="<h3>Error loading papers</h3>";
-console.log(error);
-return;
-}
+  if (error) {
+    console.log(error);
+    papers.innerHTML = "<h3>Error Loading Papers</h3>";
+    return;
+  }
 
-title.innerHTML="Papers";
+  title.innerText = "Papers";
+  papers.innerHTML = "";
 
-papers.innerHTML="";
+  if (data.length === 0) {
+    papers.innerHTML = "<h3>No Papers Found</h3>";
+    return;
+  }
 
-data.forEach(paper=>{
+  data.forEach(paper => {
 
-papers.innerHTML+=`
-<div class="card"
-onclick="location.href='chapters.html?paper=${paper.id}'">
+    papers.innerHTML += `
+      <div class="card"
+      onclick="location.href='chapters.html?paper=${paper.id}'">
 
-${paper.paper_name}
+      ${paper.paper_name}
 
-</div>
-`;
+      </div>
+    `;
 
-});
+  });
 
 }
